8d04b6c4d7ceecee6b5c81c57c7e53f7cc618bd8
Eduarda <matoseduarda420@gmail.com>
2026-05-30 22:21:14 -0300

    Adição da barra de menu

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -1,5 +1,23 @@
 import { Stack } from "expo-router";
+import { View, StyleSheet } from "react-native";
+import Menu from "../components/Menu";
+
 
 export default function Layout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
-}
+  return(
+  
+    <View style={styles.container}>
+      <Stack screenOptions={{ headerShown: false }} />
+      <Menu />
+    </View>
+  
+)}
+
+  
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#0B0B0F",
+  },
+});
